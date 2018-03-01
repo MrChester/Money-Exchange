6691dad031d0d6092ffb04411b8bd062c98cf5f2
Eugene Romankov <iromankov2010@gmail.com>
2018-03-01 08:56:26 +0300

    Add basе implementation of makeExchange() method

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    let errorMsg = { error: "You are rich, my friend! We don't have so much coins for exchange" };
+    if (currency > 10000) {
+        return errorMsg;
+    }
+
 }
