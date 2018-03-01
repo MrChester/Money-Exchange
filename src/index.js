7a6ae10be44a85c61e8477a3822ee64026938d55
Eugene Romankov <iromankov2010@gmail.com>
2018-03-01 12:15:01 +0300

    Fix index.js: Add new variables, add 'for loop' for counting coins

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    let errorMsg = { error: "You are rich, my friend! We don't have so much coins for exchange" };
+    let errorMsg = { error: "You are rich, my friend! We don't have so much coins for exchange" },
+        coinsString = ['H', 'Q', 'P', 'N', 'P'],
+        coinsValue = [50, 25, 10, 5, 1],
+        coinsCount = new Array(5).fill(0),
+        exchange = {};
+
     if (currency > 10000) {
         return errorMsg;
     }
+    if (currency <= 0) {
+        return {};
+    }
+    for (let i = 0; i < coinsValue.length; i++) {
+        while (currency - coinsValue[i] >= 0) {
+            currency -= coinsValue[i];
+            coinsCount[i]++;
+        }
+    }
 
+
+    return exchange;
 }
